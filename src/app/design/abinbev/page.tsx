b674acb7c3c5bf8c64ad553871e3255b2ad7231a
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-02-17 23:39:56 -0300

    Add cases and nav structure and clears placeholders

---
+++ src/app/design/abinbev/page.tsx
@@ -0,0 +1,3 @@
+export default function AbInbev() {
+  return <h1>ABI case</h1>
+}
--- src/components/TopNavigation/index.tsx
+++ src/components/TopNavigation/index.tsx
@@ -1,13 +1,26 @@
 import Image from "next/image";
+import MenuItem from "../MenuItem";
+import Link from "next/link";
 
 export default function Navigation() {
-  return (
-    <Image
-      src="/lucca-icon.svg"
-      alt="An illustration of Lucca holding a laptop with mountains and a galaxy sky behind."
-      width={40}
-      height={40}
-      priority
-    />
+  return (<>
+    <Link href="/">
+      <Image
+        src="/lucca-icon.svg"
+        alt="An illustration of Lucca holding a laptop with mountains and a galaxy sky behind."
+        width={40}
+        height={40}
+        priority
+      />
+    </Link>
+    Lucca Romaniello <br />
+    A brazilian Frontend Engineer with product design background. <br />
+    Open to full-time and freelance opportunities. <br /> <br />
+    Projects
+    <MenuItem />
+    <MenuItem />
+    <MenuItem />
+    <MenuItem />
+    </>
   )
 }
