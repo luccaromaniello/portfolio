b68d8c5ba88e34066fa0ee79dd0abd9628154049
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-02-18 19:52:54 -0300

    Organizes grid structure for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,12 @@ import SideNavigation from "@/components/SideNavigation";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <SideNavigation />
+    <main className="grid grid-cols-[400px_1fr] p-8">
+      <SideNavigation />
+      <div>
+        Content
+      </div>
+      {/* <div className="flex flex-row gap-8 row-start-2 items-center sm:items-start">
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
@@ -31,7 +34,7 @@ export default function Home() {
             Read our docs
           </a>
         </div>
-      </main>
-    </div>
+      </div> */}
+    </main>
   );
 }
--- src/components/SideNavigation/index.tsx
+++ src/components/SideNavigation/index.tsx
@@ -4,23 +4,24 @@ import Footer from "../Footer";
 import { MenuItem } from "../MenuItem";
 
 export default function SideNavigation() {
-  return (<>
-    <Link href="/">
-      <Image
-        src="/lucca-icon.svg"
-        alt="An illustration of Lucca holding a laptop with mountains and a galaxy sky behind."
-        width={40}
-        height={40}
-        priority
-      />
-    </Link>
-    Lucca Romaniello <br />
-    A brazilian Frontend Engineer with product design background. <br />
-    Open to full-time and freelance opportunities. <br /> <br />
-    Projects
-    <MenuItem name="AB InBev" pageReference="./design/abinbev"/>
-    <MenuItem name="Ânima Educação" pageReference="./design/anima-educacao"/>
-    <Footer />
-    </>
+  return (
+    <nav className="flex flex-col gap-8 h-full">
+      <Link href="/">
+        <Image
+          src="/lucca-icon.svg"
+          alt="An illustration of Lucca holding a laptop with mountains and a galaxy sky behind."
+          width={40}
+          height={40}
+          priority
+        />
+      </Link>
+      Lucca Romaniello <br />
+      A brazilian Frontend Engineer with product design background. <br />
+      Open to full-time and freelance opportunities. <br /> <br />
+      Projects
+      <MenuItem name="AB InBev" pageReference="./design/abinbev"/>
+      <MenuItem name="Ânima Educação" pageReference="./design/anima-educacao"/>
+      <Footer />
+    </nav>
   )
 }
