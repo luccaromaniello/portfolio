5df0e01e2ef0c89ff2e2b9c182f5e8abe39b2874
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-02-18 19:04:12 -0300

    Refactor styling structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,9 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={satoshiFont.className}
-      >
+      <body className={satoshiFont.className}>
         {children}
       </body>
     </html>
