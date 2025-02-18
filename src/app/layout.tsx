73cf3c8d41f7df2c39a50c614e0a5c7d0241b246
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-02-17 22:51:04 -0300

    Add initial styles and main font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import localFont from 'next/font/local';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const satoshiFont = localFont({
+  src: '../../public/satoshi/Satoshi-Variable.woff2',
 });
 
 export const metadata: Metadata = {
@@ -25,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={satoshiFont.className}
       >
         {children}
       </body>
