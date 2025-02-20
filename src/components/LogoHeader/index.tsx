04faa4ea36aa59b6720054efc52acbdbaf1c2abe
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-02-20 20:07:13 -0300

    Changes logo header styling

--- src/components/LogoHeader/index.tsx
+++ src/components/LogoHeader/index.tsx
@@ -3,22 +3,13 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <div className="flex flex-col gap-4">
-      <div className="flex flex-row">
-        <Link href="/">
-          {/* <Image
-            src="/lucca-icon.svg"
-            alt="An illustration of Lucca holding a laptop with mountains and a galaxy sky behind."
-            width={60}
-            height={60}
-            priority
-          /> */}
-        </Link>
-        <h1 className="text-4xl font-medium">Lucca Romaniello</h1>
+    <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-2">
+        <h1 className="text-5xl font-bold">Lucca Romaniello</h1>
+        <h2 className="text-xl font-medium text-content-secondary">Frontend Engineer with product design background</h2>
       </div>
-      <div className="flex flex-col gap-1">
-        <h2 className="text-2xl font-light">A brazilian Frontend Engineer with product design background.</h2>
-        <h3 className="text-xl font-light text-content-secondary">Open to full-time and freelance opportunities.</h3>
+      <div className="flex flex-col">
+        <h3 className="text-content-secondary">Open to full-time and freelance opportunities.</h3>
       </div>
     </div>
   )
