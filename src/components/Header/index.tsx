e7faf307925fe6fcffd4b3f3bffe8a3a33f27c4a
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-02-25 19:54:56 -0300

    Changes subtitle headline

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,7 @@ const Header = () => {
     <div className="flex flex-col gap-2">
       <div className="flex flex-col gap-2">
         <h1 className="text-5xl font-bold">Lucca Romaniello</h1>
-        <h2 className="text-xl font-medium text-content-secondary">Frontend Engineer with product design background</h2>
+        <h2 className="text-xl font-medium text-content-secondary">Frontend Engineer and former Product Designer.</h2>
       </div>
       <div className="flex flex-col">
         <h3 className="text-content-secondary">Open to full-time and freelance opportunities.</h3>
