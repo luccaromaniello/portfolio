ee3cef00eae75ccdb7f1b90af75c7b86625ad366
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-02-26 22:35:39 -0300

    Changes apostrophe warning

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,7 +3,7 @@ const About = () => {
     <div className="flex flex-col gap-6 text-base/7">
       <p>I began my career as a software developer, where I quickly realized the power of technology to bring ideas to life. As I collaborated with talented designers, I found myself drawn to the creative side of development, which led me to shift my focus towards design. Despite the shift, I continued to develop my technical expertise through a Software Engineering degree.</p>
       <p>Over time, working closely with teams to create user-focused and technically feasible solutions was rewarding, but I found myself missing the problem-solving aspect of development — the challenge of figuring out how to make things work. I missed the thrill of solving complex challenges and the satisfaction of building functional, interactive experiences from the ground up.</p>
-      <p>Now, I'm excited to fully embrace frontend development, where I can leverage both my design sensibilities and technical expertise. I thrive in environments that push me to innovate, and I’m ready to take on new challenges where I can continue learning and building impactful digital products.</p>
+      <p>Now, I&apos;m excited to fully embrace frontend development, where I can leverage both my design sensibilities and technical expertise. I thrive in environments that push me to innovate, and I’m ready to take on new challenges where I can continue learning and building impactful digital products.</p>
   </div>
   )
 };
