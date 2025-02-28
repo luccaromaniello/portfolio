2ad7bedf14e004615ccea47416e41515160781f5
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-02-27 22:46:04 -0300

    Refactor open menu button

--- src/components/navigation/TopNavigation/index.tsx
+++ src/components/navigation/TopNavigation/index.tsx
@@ -33,10 +33,14 @@ const TopNavigation = () => {
   const pathname = usePathname();
   const [isNavOpen, setIsNavOpen] = useState(false);
 
+  const handleClick = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+
   return (
     <nav className="flex flex-row w-full gap-4 justify-between py-8">
       <Logo width={40} height={40} />
-      <div>
+      <div className="flex flex-row justify-between items-center">
         <ul className="flex flex-row gap-8 hidden md:flex">
           {pages.map(({ href, title, external }) => (
             <li className="" key={title}>
@@ -44,18 +48,18 @@ const TopNavigation = () => {
             </li>
           ))}
         </ul>
-        <IoMenu className={`${isNavOpen ? 'hidden' : 'flex'} text-2xl md:hidden`} onClick={() => setIsNavOpen((prev) => !prev)}/>
+        <IoMenu className={`${isNavOpen ? 'hidden' : 'flex'} text-2xl md:hidden`} onClick={handleClick}/>
       </div>
       <div className={`${isNavOpen ? "showMobileNav" : "hideMobileNav"} px-6 pt-8`}>
         <div className="flex flex-row justify-between items-center">
           <Logo width={40} height={40} />
-          <div onClick={() => setIsNavOpen(false)}>
-            <IoClose className="text-2xl" onClick={() => setIsNavOpen(false)}/>
+          <div onClick={handleClick}>
+            <IoClose className="text-2xl"/>
           </div>
         </div>
         <ul className="flex flex-col gap-6 justify-between pb-8">
           {pages.map(({ href, title, external }) => (
-            <li key={title} onClick={() => setIsNavOpen(false)}>
+            <li key={title} onClick={handleClick}>
               <MenuItem name={title} pageReference={href} active={pathname === href} external={external} />
             </li>
           ))}
