28723248784aeff354ce7ff5a985f852edc4d199
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-02-27 21:24:39 -0300

    Add mobile navbar

--- src/components/navigation/TopNavigation/index.tsx
+++ src/components/navigation/TopNavigation/index.tsx
@@ -1,46 +1,65 @@
 'use client'
 
+import { useState } from "react";
 import { Logo } from "../../Logo";
 import { MenuItem } from "../../MenuItem";
 import { usePathname } from "next/navigation";
 import { IoMenu } from "react-icons/io5";
+import { IoClose } from "react-icons/io5";
+import "./TopNavigation.css"; 
+
+const pages = [
+  {
+    href: '/',
+    title: 'Work',
+  },
+  {
+    href: '/about',
+    title: 'About',
+  },
+  {
+    href: 'https://www.linkedin.com/in/luccaromaniello/',
+    title: 'Resumé',
+    external: true,
+  },
+  {
+    href: 'mailto:hello@luccaromaniello.com',
+    title: 'Contact',
+    external: true,
+  },
+];
 
 const TopNavigation = () => {
   const pathname = usePathname();
-
-  const pages = [
-    {
-      href: '/',
-      title: 'Work',
-    },
-    {
-      href: '/about',
-      title: 'About',
-    },
-    {
-      href: 'https://www.linkedin.com/in/luccaromaniello/',
-      title: 'Resumé',
-      external: true,
-    },
-    {
-      href: 'mailto:hello@luccaromaniello.com',
-      title: 'Contact',
-      external: true,
-    },
-  ];
+  const [isNavOpen, setIsNavOpen] = useState(false);
 
   return (
     <nav className="flex flex-row w-full gap-4 justify-between py-8">
-      <Logo width={40} height={40}/>
-      <div>
+      <Logo width={40} height={40} />
+      <div id="desktop-menu">
         <ul className="flex flex-row gap-8 hidden md:flex">
           {pages.map(({ href, title, external }) => (
             <li className="" key={title}>
-              <MenuItem name={title} pageReference={href} active={pathname === href} external={external}/>
+              <MenuItem name={title} pageReference={href} active={pathname === href} external={external} />
             </li>
           ))}
         </ul>
-        <IoMenu className="text-2xl flex md:hidden"/>
+        <IoMenu className={`${isNavOpen ? 'hidden' : 'flex'} text-2xl md:hidden`} onClick={() => setIsNavOpen((prev) => !prev)}/>
+      </div>
+      <div id="mobile-menu-expanded" className={`${isNavOpen ? "showMobileNav" : "hideMobileNav"} px-4`}>
+        <div className="flex flex-row justify-between items-center">
+          <Logo width={40} height={40} />
+          <div onClick={() => setIsNavOpen(false)}>
+            <IoClose className="text-2xl" onClick={() => setIsNavOpen(false)}/>
+          </div>
+        </div>
+        <ul className="flex flex-col gap-6 justify-between">
+          {pages.map(({ href, title, external }) => (
+            <li key={title}>
+              <MenuItem name={title} pageReference={href} active={pathname === href} external={external} />
+            </li>
+          ))}
+        </ul>
       </div>
     </nav>
   )
---
+++ src/components/navigation/index.tsx
@@ -0,0 +1,44 @@
+import { usePathname } from "next/navigation";
+
+const TopNavigation = () => {
+  const pathname = usePathname();
+
+  const pages = [
+    {
+      href: '/',
+      title: 'Work',
+    },
+    {
+      href: '/about',
+      title: 'About',
+    },
+    {
+      href: 'https://www.linkedin.com/in/luccaromaniello/',
+      title: 'Resumé',
+      external: true,
+    },
+    {
+      href: 'mailto:hello@luccaromaniello.com',
+      title: 'Contact',
+      external: true,
+    },
+  ];
+
+  return (
+    <nav className="flex flex-row w-full gap-4 justify-between py-8">
+      <Logo width={40} height={40}/>
+      <div>
+        <ul className="flex flex-row gap-8 hidden md:flex">
+          {pages.map(({ href, title, external }) => (
+            <li className="" key={title}>
+              <MenuItem name={title} pageReference={href} active={pathname === href} external={external}/>
+            </li>
+          ))}
+        </ul>
+        <IoMenu className="text-2xl flex md:hidden"/>
+      </div>
+    </nav>
+  )
+};
+
+export default TopNavigation;
