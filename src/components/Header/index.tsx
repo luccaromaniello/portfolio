3d0122489bbce8b29c093a03b1b7c520ffc881b0
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-02-27 22:03:16 -0300

    Changes text alignment of header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <div className="flex flex-col gap-6 py-12 text-center">
+    <div className="flex flex-col gap-6 py-12 text-left sm:text-center">
       <div className="flex flex-col gap-4">
         <div className="flex flex-col gap-3">
           <h1 className="text-3xl sm:text-5xl leading-tight sm:leading-snug font-medium">I&apos;m Lucca Romaniello, a Frontend Engineer with product design background.</h1>
