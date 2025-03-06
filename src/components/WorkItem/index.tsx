8c90cd119dc8895e49a51c8d24318e4945277cc2
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-03-06 20:47:48 -0300

    Add layout structure for workitems

--- src/components/WorkItem/index.tsx
+++ src/components/WorkItem/index.tsx
@@ -10,7 +10,7 @@ interface WorkItemProps {
 
 const WorkItem = ({ startDate, endDate, role, company, sector, location, current }: WorkItemProps) => {
   return (
-    <div className={`flex flex-row justify-between${!current ? 'border-t': ''}`}>
+    <div className={`flex flex-row w-full justify-items-start justify-between items-center py-4 ${!current ? 'border-t': ''}`}>
       <p className="text-content-secondary">
         {startDate} - {current ? <p className="text-primary-main font-medium">now</p> : <p>{endDate}</p>}
       </p>
