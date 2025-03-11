4efddd9d835fa211f4f1ece1849a92b59d02dae7
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-03-10 22:55:47 -0300

    Adjust flex sizing to work items

--- src/components/WorkItem/index.tsx
+++ src/components/WorkItem/index.tsx
@@ -12,12 +12,12 @@ interface WorkItemProps {
 const WorkItem = ({ startDate, endDate, role, company, sector, description, location, current }: WorkItemProps) => {
   return (
     <div className={`flex flex-row w-full justify-between py-4 gap-12 ${!current ? 'border-t': ''}`}>
-      <div className="flex flex-col w-full max-w-fit gap-1">
+      <div className="flex flex-col basis-48 shrink-0 gap-1">
         <p className="">{location}</p>
         <p className="text-content-secondary">{startDate} - {current ? <span className="text-primary-main font-medium">now</span> : <span>{endDate}</span>}</p>
       </div>
-      <div className="flex flex-col gap-2">
-        <p className="w-full"><span className="font-medium">{role}</span> — <span className="text-base font-medium text-primary-main hover:text-content-tertiary">{company}</span></p>
+      <div className="flex flex-col grow gap-2">
+        <p><span className="font-medium">{role}</span> — <span className="text-base font-medium text-primary-main hover:text-content-tertiary">{company}</span></p>
         <p className="text-content-secondary">{description}</p>
         {/* <p>{sector}</p> */}
       </div>
