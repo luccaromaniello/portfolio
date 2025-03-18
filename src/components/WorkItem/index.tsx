ada36c0d140bf491e1e494fa486ca9b4c3e33044
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-03-18 18:14:45 -0300

    Add tags to work items in about section

--- src/components/WorkItem/index.tsx
+++ src/components/WorkItem/index.tsx
@@ -1,3 +1,5 @@
+import Chip from "../Chip";
+
 interface WorkItemProps {
   startDate: string,
   endDate?: string,
@@ -5,12 +7,15 @@ interface WorkItemProps {
   company: string,
   website: string,
   sector: string,
+  technologies?: string[],
   description: string,
   location: string,
   current: boolean,
 }
 
-const WorkItem = ({ startDate, endDate, role, company, website, sector, description, location, current }: WorkItemProps) => {
+const WorkItem = ({ startDate, endDate, role, company, website, sector, technologies, description, location, current }: WorkItemProps) => {
+  const chips = technologies?.map((technology, i) => <Chip key={i} text={technology}/>)
+
   return (
     <div className={`flex flex-row w-full justify-between py-4 gap-12 ${!current ? 'border-t': ''}`}>
       <div className="flex flex-col basis-48 shrink-0 gap-1">
@@ -20,7 +25,10 @@ const WorkItem = ({ startDate, endDate, role, company, website, sector, descript
       <div className="flex flex-col grow gap-2">
         <p><span className="font-medium">{role}</span> — <a href={website} rel="noreferrer" target="_blank" className="text-base font-medium text-primary-main hover:text-content-tertiary">{company}</a></p>
         <p className="text-content-secondary">{description}</p>
-        {/* <p>{sector}</p> */}
+        <div className="flex flex-row gap-2 py-2">
+          <Chip text={sector} /> 
+          {chips}
+        </div>
       </div>
     </div>
   )
