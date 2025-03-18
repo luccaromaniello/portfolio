131f09a88def6d9489e766b77df6f81a0f37597e
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-03-18 18:27:45 -0300

    Changes about description and add lucca's photo

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,15 +1,28 @@
 import EducationItem from "@/components/EducationItem";
 import WorkItem from "@/components/WorkItem";
+import Image from "next/image";
 
 const About = () => {
   return (
     <div className="flex flex-col gap-16">
-      <div className="flex flex-col gap-6 text-base/7">
-        <h1 className="text-3xl sm:text-5xl leading-tight sm:leading-snug font-medium">I&apos;m currently a Product Designer at AB InBev.</h1>
-        <div className="flex flex-col gap-6 text-lg text-content-secondary">
-          <p>I began my career as a software developer, where I quickly realized the power of technology to bring ideas to life. As I collaborated with talented designers, I found myself drawn to the creative side of development, which led me to shift my focus towards design. Despite the shift, I continued to develop my technical expertise through a Software Engineering degree.</p>
-          <p>Over time, working closely with teams to create user-focused and technically feasible solutions was rewarding, but I found myself missing the problem-solving aspect of development — the challenge of figuring out how to make things work. I missed the thrill of solving complex challenges and the satisfaction of building functional, interactive experiences from the ground up.</p>
-          <p>Now, I&apos;m excited to fully embrace frontend development, where I can leverage both my design sensibilities and technical expertise. I thrive in environments that push me to innovate, and I’m ready to take on new challenges where I can continue learning and building impactful digital products.</p>
+      <div className="flex flex-row gap-32">
+        <div className="flex flex-col gap-6 text-base/7">
+          <h1 className="text-3xl sm:text-5xl leading-tight sm:leading-snug font-medium">I&apos;m currently a Product Designer at AB InBev.</h1>
+          <div className="flex flex-col gap-6 text-lg text-content-secondary">
+            <p>I began my career as a software developer, where I quickly realized the power of technology to bring ideas to life. As I collaborated with talented designers, I found myself drawn to the creative side of development, which led me to shift my focus towards design. Despite the shift, I continued to develop my technical expertise through a Software Engineering degree.</p>
+            <p>Over time, working closely with teams to create user-focused solutions was rewarding, but I found myself missing the problem-solving aspect of development — the challenge of figuring out how to make things work. I missed the thrill of solving complex challenges and the satisfaction of building functional, interactive experiences from stratch.</p>
+            <p>Now, I&apos;m excited to fully embrace frontend development, where I can leverage both my design sensibilities and technical expertise. I thrive in environments that push me to innovate, and I’m ready to take on new challenges where I can continue learning and building impactful digital products.</p>
+          </div>
+        </div>
+        <div className="rounded-3xl">
+          <Image
+            src="/lucca-profile.png"
+            style={{objectFit: "contain"}}
+            width={900}
+            height={900}
+            alt="Picture of Lucca Romaniello."
+            priority
+          />
         </div>
       </div>
       <div className="flex flex-col sm:gap-8 border-t">
