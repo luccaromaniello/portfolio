c8b1f41634191eb5759fe2020e1670b639d91020
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-03-20 20:06:59 -0300

    Adds base structure for side menu case links

--- src/app/design/anima-educacao/page.tsx
+++ src/app/design/anima-educacao/page.tsx
@@ -1,7 +1,10 @@
+import SideMenuAnima from "@/components/cases/SideMenuAnima";
+
 export default function AnimaEducacao() {
   return (
     <div>
       <div>
+        <SideMenuAnima></SideMenuAnima>
         This is Ânima Educação Content
       </div>
     </div>
---
+++ src/components/cases/SideMenuAnima/index.tsx
@@ -0,0 +1,18 @@
+const SideMenuAnima = () => {
+  const linkStyle = "text-sm font-bold text-content-tertiary hover:text-content-primary";
+    
+  return (
+    <div className="flex flex-col gap-4">
+      <a href="" className={linkStyle}>Overview</a>
+      <a href="" className={linkStyle}>Challenge</a>
+      <a href="" className={linkStyle}>Research</a>
+      <a href="" className={linkStyle}>Prototypes</a>
+      <a href="" className={linkStyle}>Solution</a>
+      <a href="" className={linkStyle}>Results</a>
+      <a href="" className={linkStyle}>Design system</a>
+      <a href="" className={linkStyle}>Documentation</a>
+    </div>
+  );
+}
+
+export default SideMenuAnima;
