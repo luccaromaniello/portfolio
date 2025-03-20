765f22e690de0009580b3e2a390139f24eb00bed
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-03-20 20:24:16 -0300

    Changes folder structure to add multiple layouts

---
+++ src/app/(projects)/design/anima-educacao/page.tsx
@@ -0,0 +1,31 @@
+import SideMenuAnima from "@/components/cases/SideMenuAnima";
+import EducationItem from "@/components/EducationItem";
+import WorkItem from "@/components/WorkItem";
+
+export default function AnimaEducacao() {
+  return (
+    <div className="flex flex-col">
+      <SideMenuAnima></SideMenuAnima>
+      <div className="flex flex-col gap-4 sm:gap-8 border-t">
+        <h2 className="text-3xl sm:text-4xl pt-8">Experience</h2>
+        <div className="flex flex-col w-full pb-8">
+          <WorkItem startDate="Sep 22" role="Product Designer" company="AB InBev" website="https://www.ab-inbev.com/" sector="E-commerce/SaaS" technologies={["User experience", "User research", "Figma"]} description="AB InBev is a global beverage and brewing leader, and owns brands like Budweiser, Corona, and Stella Artois. I led a developer web portal redesign with cross-functional teams, boosting system usability by 9.63%, user satisfaction by 9.21%, and task efficiency by 13.97% in 3 months. I also led development of multiple features for other apps including admin tools, ERP integration, order and deliveries management, resulting in increased efficiency and cost savings." location="Remote" current />
+          <WorkItem 
+            startDate="Aug 21" endDate="Feb 22" role="Product Designer" company="fluke" website="https://flu.ke/" sector="Healthcare" technologies={["Design workshops", "Customer experience", "Data visualization"]} description="fluke is a brazilian Mobile Virtual Network Operator focused on simplifying processes that are commonly bureaucratic in traditional mobile operators in Brazil. I was responsible for multiple improvements of the SIM card acquisition flow, which resulted in a user contact rate
+reduction and increased customer satisfaction." location="Remote" current={false} />
+          <WorkItem startDate="Feb 22" endDate="Sep 22" role="Product Designer" company="Conexa Saúde" website="https://www.conexasaude.com.br/" sector="Mobile network" technologies={["Prototyping", "User interviews"]} description="Conexa Saúde is recognized as the largest integrated healthcare platform in Latin America, facilitating connections between patients, doctors, and healthcare institutions through telemedicine services. I contributed to multiple improvements in the doctors' experience, actively participating from conception to implementation of various features." location="Remote" current={false} />
+          <WorkItem startDate="Jul 20" endDate="Aug 21" role="Product Designer" company="dti digital + Ânima Educação" website="https://www.dtidigital.com.br/" sector="Higher education" technologies={["UI/UX design", "Design systems", "Usability testing"]} description="Ânima Educação is a company responsible for managing multiple private universities within Brazil. I helped to build a web platform that allowed their internal staff from Ânima to generate Bachelor’s and Master’s certificates for their 350.000+ students, following the Brazilian Ministry of Education requirements." location="Remote" current={false} />
+          <WorkItem startDate="Apr 19" endDate="Jul 20" role="Full Stack Developer" company="dti digital + Localiza&Co" website="https://www.dtidigital.com.br/" sector="Urban mobility" technologies={["React.js", "Vue.js", "ASP.NET", "Kotlin", "Flutter"]} description="Localiza&Co offers complete mobility solutions for individuals and businesses, including car rentals, car subscriptions, fleet outsourcing, and sales of vehicles, operating across 7 countries in Latin America through franchises. There, I was a mobile and backend developer for an app migration from hybrid to native used by thier carport operators. I also worked a frontend developer for an internal software used for visual management and merit money." location="Belo Horizonte, Brazil" current={false} />
+        </div>
+      </div>
+      <div className="flex flex-col gap-4 sm:gap-8 pb-20 sm:pb-24 border-t">
+        <h2 className="text-3xl sm:text-4xl pt-8">Education</h2>
+        <div className="flex flex-col w-full">
+          <EducationItem description="Self-taught: always learning from every book, article, co-worker, and life experience" location="Anywhere" always/>
+          <EducationItem description="Full Stack Development, Postgraduate degree (Lato sensu)" school="Pontifícia Católica Universidade de Minas Gerais" website="https://www.linkedin.com/school/pucminas/" startDate="2024" endDate="2025" location="Remote" always={false}/>
+          <EducationItem description="Software Engineering, Bachelor’s degree" school="Pontifícia Católica Universidade de Minas Gerais" startDate="2018" website="https://www.linkedin.com/school/pucminas/" endDate="2022" location="Belo Horizonte, Brazil" always={false}/>
+        </div>
+      </div>
+    </div>
+  )
+}
