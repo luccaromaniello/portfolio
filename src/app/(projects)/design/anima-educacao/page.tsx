cb4a1052b84506adc468bd90b857a23242520b45
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-03-20 22:37:39 -0300

    Changes font size of case heading

--- src/app/(projects)/design/anima-educacao/page.tsx
+++ src/app/(projects)/design/anima-educacao/page.tsx
@@ -6,7 +6,7 @@ export default function AnimaEducacao() {
     <div className="flex flex-col">
       <SideMenuAnima></SideMenuAnima>
       <div className="flex flex-col gap-4 px-6 lg:px-56 2xl:px-96  w-full">
-        <h1 className="text-3xl sm:text-5xl leading-tight sm:leading-snug font-bold pt-8">I helped <CaseHeading text="Ânima Educação" link="https://www.linkedin.com/company/animaeducacao/"/> to build a web platform that is used to generate Bachelor&apos;s and Master&apos;s certificates for more than 350.000 students in Brazil.</h1>
+        <h1 className="text-3xl sm:text-4xl leading-tight sm:leading-snug font-bold pt-8">I helped <CaseHeading text="Ânima Educação" link="https://www.linkedin.com/company/animaeducacao/"/> to build a web platform that is used to generate Bachelor&apos;s and Master&apos;s certificates for more than 350.000 students in Brazil.</h1>
       </div>
     </div>
   )
