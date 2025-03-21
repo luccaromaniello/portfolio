9a59b847f5675c0a290d13bd732ae37f1e7c3a16
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-03-20 23:03:54 -0300

    Add overview component without styling

--- src/app/(projects)/design/anima-educacao/page.tsx
+++ src/app/(projects)/design/anima-educacao/page.tsx
@@ -1,5 +1,6 @@
 import SideMenuAnima from "@/components/cases/SideMenuAnima";
 import CaseHeading from "@/components/cases/CaseHeading";
+import CaseOverview from "@/components/cases/CaseOverview";
 
 export default function AnimaEducacao() {
   return (
@@ -7,6 +8,13 @@ export default function AnimaEducacao() {
       <SideMenuAnima></SideMenuAnima>
       <div className="flex flex-col gap-4 px-6 lg:px-56 2xl:px-96  w-full">
         <CaseHeading headline="build a web platform that is used to generate Bachelor&apos;s and Master&apos;s certificates for more than 350.000 students in Brazil." company="Ânima Educação" link="https://www.linkedin.com/company/animaeducacao/"/>
+        <CaseOverview overview={`Ânima Educação is a company responsible for managing multiple private universities within Brazil. In 2020, they realized that its platform could be restructured to attend not only Ânima itself, but other higher education organizations.
+          This was possible since the Ministry of Education requirements should be followed by any organization that provides certificates to their students.`}
+          date="August 2020 - July 2021"
+          role="Product Designer — dti digital + Ânima Educação"
+          keywords="User research, visual design, usability testing."
+          results="Delivered the new platform to Ânima, which by 2021 was used to attend around 350.000 students."
+          />
       </div>
     </div>
   )
