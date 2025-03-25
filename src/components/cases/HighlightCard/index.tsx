7411c4b0bef8580076038e4e0b36120779f5f415
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-03-25 18:14:56 -0300

    Add web style to HighlightCard component

---
+++ src/components/cases/HighlightCard/index.tsx
@@ -0,0 +1,17 @@
+import Text from "@/components/cases/Text";
+
+interface HighlightCardProps {
+  title: string,
+  description: string,
+}
+
+const HighlightCard = ({ title, description }: HighlightCardProps) => {
+  return (
+    <div className="flex flex-col gap-4 p-8 bg-primary-main/5 rounded-2xl">
+      <h2 className="text-2xl font-bold">{title}</h2>
+      <Text text={description}/>
+    </div>
+  )
+};
+
+export default HighlightCard;
