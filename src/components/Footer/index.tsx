6038f93d34a29db93c170be1b1f38649920ec801
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-03-25 19:55:24 -0300

    Styles footer and add links as constants

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,15 +1,22 @@
-import { FaGithub } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa";
-import { FaEnvelope } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa6";
+import { FaEnvelope } from "react-icons/fa6";
+import { GITHUB_URL, LINKEDIN_URL, MAIL_URL } from "@/constants";
 
 const Footer = () => {
   return (
-    <footer className="flex flex-col gap-4 items-center p-14 w-full">
-      {/* <div className="flex flex-row gap-4">
-        <FaGithub className="text-2xl hover:text-content-tertiary"/>
-        <FaLinkedin className="text-2xl hover:text-content-tertiary"/>
-        <FaEnvelope className="text-2x1 hover:text-content-tertiary"/>
-       </div> */}
+    <footer className="flex flex-col gap-4 items-center p-14 w-full items-center">
+      <div className="flex flex-row gap-6 items-center">
+        <a href={GITHUB_URL} rel="noreferrer" target="_blank">
+          <FaGithub size={24} className="text-2xl hover:text-content-tertiary" />
+        </a>
+        <a href={LINKEDIN_URL} rel="noreferrer" target="_blank">
+          <FaLinkedin size={24} className="text-2xl hover:text-content-tertiary" />
+        </a>
+        <a href={MAIL_URL}>
+          <FaEnvelope size={24} className="text-3x1 hover:text-content-tertiary" />
+        </a>
+      </div>
       <div className="flex flex-col gap-2 text-xs text-content-secondary items-center">
         <p>Designed and coded by Lucca Romaniello.</p>
         <p>Last updated April 2025.</p>
---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const GITHUB_URL = "https://github.com/luccaromaniello/";
+export const LINKEDIN_URL = "https://www.linkedin.com/in/luccaromaniello/";
+export const MAIL_URL = "mailto:hello@luccaromaniello.com";
