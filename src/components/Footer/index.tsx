d59bf0d66e1824eb45a43b9582dc4d47e2673dd9
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-03-25 19:40:24 -0300

    Changes footer styling

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,15 +4,15 @@ import { FaEnvelope } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className="flex flex-col gap-4">
-      <div className="flex flex-row gap-4 items-center">
+    <footer className="flex flex-col gap-4 items-center p-14 w-full">
+      {/* <div className="flex flex-row gap-4">
         <FaGithub className="text-2xl hover:text-content-tertiary"/>
         <FaLinkedin className="text-2xl hover:text-content-tertiary"/>
         <FaEnvelope className="text-2x1 hover:text-content-tertiary"/>
-       </div>
-      <div className="flex flex-col gap-2 text-xs text-content-secondary">
-        <p>Designed and coded using Next.js and Tailwind CSS.</p>
-        <p>Last updated March 2025.</p>
+       </div> */}
+      <div className="flex flex-col gap-2 text-xs text-content-secondary items-center">
+        <p>Designed and coded by Lucca Romaniello.</p>
+        <p>Last updated April 2025.</p>
       </div>
     </footer>
   )
