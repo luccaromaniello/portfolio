9fac72db7428d4af0dd18c4c524db456cf779af1
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-03-26 18:42:47 -0300

    Add gap to section heading content

--- src/components/cases/typography/SectionHeading/index.tsx
+++ src/components/cases/typography/SectionHeading/index.tsx
@@ -7,7 +7,7 @@ interface SectionHeadingProps {
 
 const SectionHeading = ({ title, text }: SectionHeadingProps) => {
   return (
-    <div>
+    <div className="flex flex-col gap-4">
       <h3 className="text-lg sm:text-2xl font-bold leading-snug sm:leading-normal">
         {title}
       </h3>
