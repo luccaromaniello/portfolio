b4253b40b6f01cca74dd858d44bff036c9e7c6bd
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-03-26 18:50:37 -0300

    Refactor text component folder structure

--- src/components/cases/HighlightCard/index.tsx
+++ src/components/cases/HighlightCard/index.tsx
@@ -1,17 +1,19 @@
-import Text from "@/components/cases/Text";
+import Text from "@/components/cases/typography/Text";
 
 interface HighlightCardProps {
-  title: string,
-  description: string,
+  title: string;
+  description: string;
 }
 
 const HighlightCard = ({ title, description }: HighlightCardProps) => {
   return (
     <div className="flex flex-col gap-6 p-6 sm:p-16 sm:my-12 bg-primary-main/5 rounded-2xl">
-      <h2 className="text-2xl sm:text-3xl font-bold leading-snug sm:leading-normal">{title}</h2>
-      <Text text={description}/>
+      <h2 className="text-2xl sm:text-3xl font-bold leading-snug sm:leading-normal">
+        {title}
+      </h2>
+      <Text text={description} />
     </div>
-  )
+  );
 };
 
 export default HighlightCard;
