3266ab1bfec8b1874ad808aaf53d80c97a356b25
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-03-31 21:34:16 -0300

    Changes cards border radius to be a constant

--- src/constants.ts
+++ src/constants.ts
@@ -2,4 +2,5 @@ export const GITHUB_URL = "https://github.com/luccaromaniello/";
 export const LINKEDIN_URL = "https://www.linkedin.com/in/luccaromaniello/";
 export const MAIL_URL = "mailto:hello@luccaromaniello.com";
 export const DEFAULT_ALIGNMENT =
-    "flex flex-col gap-20 px-6 lg:px-56 2xl:px-96 w-full";
+  "flex flex-col gap-20 px-6 lg:px-56 2xl:px-96 w-full";
+export const DEFAULT_CARD_RADIUS = "rounded-2xl";
