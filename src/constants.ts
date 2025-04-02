b6f40bdb89a1d800fc240e84a47e4ddf4fcab317
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-04-02 00:16:02 -0300

    Fixes padding responsiveness of imageCard

--- src/constants.ts
+++ src/constants.ts
@@ -4,5 +4,5 @@ export const MAIL_URL = "mailto:hello@luccaromaniello.com";
 export const DEFAULT_ALIGNMENT =
   "flex flex-col gap-20 px-6 lg:px-56 2xl:px-96 w-full";
 export const CARD_RADIUS = "rounded-2xl";
-export const CARD =
-  "p-12 shadow-[0_4px_16px_rgba(17,17,26,0.10)]" + " " + CARD_RADIUS;
+export const CARD_PADDING = "p-8 sm:p-12";
+export const CARD_SHADOW = "shadow-[0_4px_16px_rgba(17,17,26,0.10)]";
