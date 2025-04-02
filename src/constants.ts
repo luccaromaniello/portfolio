e49c7d8a137026d03286189bfb4b95004fd0fe2d
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-04-01 23:42:08 -0300

    Changes card padding when responsive

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,6 @@ export const LINKEDIN_URL = "https://www.linkedin.com/in/luccaromaniello/";
 export const MAIL_URL = "mailto:hello@luccaromaniello.com";
 export const DEFAULT_ALIGNMENT =
   "flex flex-col gap-20 px-6 lg:px-56 2xl:px-96 w-full";
-export const CARD_PADDING = "p-6 sm:py-12 sm:px-16";
 export const CARD_RADIUS = "rounded-2xl";
-export const CARD_SHADOW = "shadow-[0_4px_16px_rgba(17,17,26,0.10)]";
-export const CARD = CARD_PADDING + " " + CARD_RADIUS + " " + CARD_SHADOW;
+export const CARD =
+  "p-12 shadow-[0_4px_16px_rgba(17,17,26,0.10)]" + " " + CARD_RADIUS;
