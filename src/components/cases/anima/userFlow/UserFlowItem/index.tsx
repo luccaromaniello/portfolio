98efca9fe57781de7738b1ea894e2dfe295f4107
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-04-03 18:02:56 -0300

    Adds base for styling user flow items

--- src/components/cases/anima/userFlow/UserFlowItem/index.tsx
+++ src/components/cases/anima/userFlow/UserFlowItem/index.tsx
@@ -1,4 +1,5 @@
 import Text from "@/components/cases/typography/Text";
+import { CARD_RADIUS } from "@/constants";
 
 interface UserFlowItemProps {
   heading: string;
@@ -18,16 +19,24 @@ const UserFlowItem = ({
   personas,
 }: UserFlowItemProps) => {
   return (
-    <div className="flex flex-row gap-16">
-      <div className="flex flex-col w-1/3">
+    <div
+      className={`${CARD_RADIUS} flex flex-row gap-16 border border-dashed border-primary-main/35`}
+    >
+      <div className="flex flex-col w-1/3 px-6 justify-items-center">
         <h5>{heading}</h5>
-        <Text text={description ? description : ""} />
+        <Text text={description ? description : ""} type={1} />
       </div>
-      <div className="flex flex-row">
-        <div>Avatar</div>
-        <div className="flex flex-col">
-          <Text text={personas.action} />
-        </div>
+      <div className="flex flex-row w-full">
+        {personas.map((persona, index) => (
+          <div key={index}>
+            <div>Avatar</div>
+            <div className="flex flex-col">
+              <p>{persona.name}</p>
+              <p>{persona.role}</p>
+              <Text text={persona.action} />
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
