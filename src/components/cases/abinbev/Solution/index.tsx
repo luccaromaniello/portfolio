bdd20d58ec62f3d22dc3c419d50ed2a87a55c4ae
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-04-21 18:29:56 -0300

    Starts solution section

---
+++ src/components/cases/abinbev/Solution/index.tsx
@@ -0,0 +1,15 @@
+import SectionHeading from "@/components/cases/typography/SectionHeading";
+import "@/styles/alignments.css";
+
+const Solution = () => {
+  return (
+    <div className="section-container">
+      <SectionHeading
+        title="Solution"
+        text="The updated BEES developer portal unifies documentation into a single reference, addressing issues with Stoplight. It centralizes information to promote consistency and simplify integration for both internal teams and partners."
+      />
+    </div>
+  );
+};
+
+export default Solution;
