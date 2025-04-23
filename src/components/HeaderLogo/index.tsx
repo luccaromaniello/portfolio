c7e6f26e73dc8b0668a45ad6c73fd025d1d2aa59
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-04-23 20:59:54 -0300

    Adds behavior for clip path image on cursor

--- src/components/HeaderLogo/index.tsx
+++ src/components/HeaderLogo/index.tsx
@@ -1,17 +1,68 @@
+"use client";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 
 const HeaderLogo = () => {
+  const [mousePosition, setMousePosition] = useState<{ x: number; y: number }>({
+    x: 0,
+    y: 0,
+  });
+
+  useEffect(() => {
+    const container = document.getElementById("header-logo-container");
+    const handleMouseMove = (event: MouseEvent) => {
+      if (!container) return;
+      const rect = container.getBoundingClientRect();
+      const relativeX = event.clientX - rect.left;
+      const relativeY = event.clientY - rect.top;
+      const percentX = (relativeX / rect.width) * 100;
+      const percentY = (relativeY / rect.height) * 100;
+
+      setMousePosition({
+        x: percentX,
+        y: percentY,
+      });
+    };
+
+    container?.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      container?.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
   return (
-    <div className="after:absolute after:top-0 after:left-0 after:w-full after:h-full after:opacity-0 after:bg-[url('/lucca-overlay-bw.png')] after:bg-cover after:bg-center after:transition-opacity after:duration-300 after:ease-out hover:after:opacity-100 after:[clip-path:circle(15%_at_50%_50%)]">
+    <div id="header-logo-container" className="relative">
       <Image
         src="/lucca-icon.png"
         alt="Picture of Lucca Romaniello."
         priority
-        className="object-contain rounded-lg object-top w-full h-full"
+        className="object-contain rounded-lg object-top w-full h-full relative cursor-default z-1"
         width={360}
         height={480}
         sizes="100%"
       />
+      {/* <Image
+        src="/lucca-overlay-bw.png"
+        alt="Picture of Lucca Romaniello."
+        priority
+        className="object-contain rounded-lg object-top w-full h-full absolute top-0 left-0"
+        width={360}
+        height={480}
+        sizes="100%"
+      /> */}
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          background: "black",
+          pointerEvents: "none",
+          clipPath: `circle(10% at ${mousePosition.x.toFixed(0)}% ${mousePosition.y.toFixed(0)}%)`,
+        }}
+      ></div>
     </div>
   );
 };
