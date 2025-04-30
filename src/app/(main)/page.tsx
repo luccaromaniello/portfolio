53d1a608a8a54e53683fbf47437f15163eca510e
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-04-29 22:09:26 -0300

    Updates spotlight positioning

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -15,15 +15,21 @@ const Home = () => {
     const backgroundContainer = document.getElementById("background-container");
     const handleMouseMove = (event: MouseEvent) => {
       if (!backgroundContainer) return;
-      const x = event.pageX;
-      const y = event.pageY;
-      // const rect = backgroundContainer.getBoundingClientRect();
-      // const relativeX = event.clientX - rect.left;
-      // const relativeY = event.clientY - rect.top;
-      // const percentX = (relativeX / rect.width) * 100;
-      // const percentY = (relativeY / rect.height) * 100;
+      const { left, top } = backgroundContainer.getBoundingClientRect();
+      const spotlightSize = 300;
+      const x = event.pageX - left - spotlightSize / 2;
+      const y = event.pageY - top - spotlightSize / 2;
 
-      setMousePosition({ x: x, y: y });
+      setMousePosition({
+        x: Math.max(
+          0,
+          Math.min(x, backgroundContainer.offsetWidth - spotlightSize),
+        ),
+        y: Math.max(
+          0,
+          Math.min(y, backgroundContainer.offsetHeight - spotlightSize),
+        ),
+      });
     };
 
     backgroundContainer?.addEventListener("mousemove", handleMouseMove);
@@ -39,19 +45,21 @@ const Home = () => {
         id="background-container"
         className="flex flex-col px-6 lg:px-56 2xl:px-96 w-full min-h-96 relative overflow-hidden"
       >
-        <div className="absolute inset-0 bg-[url('/background-galaxy.png')] bg-cover bg-center bg-repeat z-0 brightness-50" />
+        <div className="absolute inset-0 bg-[url('/background-galaxy.png')] bg-cover bg-center bg-repeat z-0 brightness-50 hover:cursor-[url()]" />
+        <div
+          id="spotlight"
+          style={{
+            top: `${mousePosition.y}px`,
+            left: `${mousePosition.x}px`,
+            backgroundImage: `radial-gradient(circle, rgba(255, 255, 255, 0.08) 0%, transparent 50%, rgba(0, 0, 0, 0.6) 100%)`,
+            pointerEvents: "none",
+            mixBlendMode: "lighten",
+            width: "300px",
+            height: "300px",
+          }}
+          className="absolute z-10 rounded-full"
+        />
         <div className="relative z-10">
-          <div
-            id="spotlight"
-            style={{
-              top: `${mousePosition.y - 64}px`,
-              left: `${mousePosition.x - 64}px`,
-              backgroundImage: `radial-gradient(circle, rgba(255, 255, 255, 0.08) 0%, transparent 50%, rgba(0, 0, 0, 0.6) 100%)`,
-              pointerEvents: "none",
-              mixBlendMode: "lighten",
-            }}
-            className="absolute w-full h-full z-10 rounded-full"
-          />
           <TopNavigation darkTheme />
           <Header />
         </div>
