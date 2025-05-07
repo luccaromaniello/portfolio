ccbeae8ca04569d7a38fd991a65174c43201530a
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-05-07 17:54:29 -0300

    Changes display logic to add hover effect

--- src/components/main/DesignArchive/DesignArchive.tsx
+++ src/components/main/DesignArchive/DesignArchive.tsx
@@ -52,28 +52,23 @@ const DesignArchive = () => {
         Design archive
       </h3>
       <div className="grid grid-cols-1 grid-rows-4 sm:grid-cols-2 sm:grid-rows-2 gap-y-12 gap-x-16 sm:gap-y-8 sm:justify-between">
-        {projects.map(
-          (
-            { title, company, description, link, externalLink, image },
-            index,
-          ) => (
-            <div
-              key={index}
-              className={`relative transition-opacity duration-300 ${hoveredIndex !== null && hoveredIndex !== index ? "opacity-40" : "opacity-100"}`}
-              onMouseEnter={() => setHoveredIndex(index)}
-              onMouseLeave={() => setHoveredIndex(null)}
-            >
-              <ProjectCard
-                title={title}
-                company={company}
-                description={description}
-                link={link}
-                externalLink={externalLink ? externalLink : ""}
-                image={image ?? image}
-              />
-            </div>
-          ),
-        )}
+        {projects.map(({ title, company, description, link, image }, index) => (
+          <div
+            key={index}
+            className={`relative transition-opacity duration-300 ${hoveredIndex !== null && hoveredIndex !== index ? "opacity-40" : "opacity-100"}`}
+            onMouseEnter={() => setHoveredIndex(index)}
+            onMouseLeave={() => setHoveredIndex(null)}
+          >
+            <ProjectCard
+              title={title}
+              company={company}
+              description={description}
+              link={link}
+              // externalLink={externalLink ? externalLink : ""}
+              image={image ?? image}
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
