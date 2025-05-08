f823d7a587f0af7be0373397c41818e4b7d7cad3
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-05-08 20:41:12 -0300

    Updates footer icons styling

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,30 +1,27 @@
-import { FaGithub } from "react-icons/fa6";
-import { FaLinkedin } from "react-icons/fa6";
-import { FaEnvelope } from "react-icons/fa6";
+import { IoLogoGithub } from "react-icons/io5";
+import { IoLogoLinkedin } from "react-icons/io5";
+import { IoMail } from "react-icons/io5";
 import { GITHUB_URL, LINKEDIN_URL, MAIL_URL } from "@/utils/constants";
 import "@/styles/typography.css";
 
 const Footer = () => {
   return (
     <footer className="flex flex-col gap-4 items-center p-14 w-full sm:mt-12 mb-4">
-      <div className="flex flex-row gap-6 items-center">
+      <div className="flex flex-row gap-6 items-center text-content-secondary">
         <a href={GITHUB_URL} rel="noreferrer" target="_blank">
-          <FaGithub
+          <IoLogoGithub
             size={24}
-            className="text-2xl hover:text-content-tertiary"
+            className="text-2xl hover:text-content-primary"
           />
         </a>
         <a href={LINKEDIN_URL} rel="noreferrer" target="_blank">
-          <FaLinkedin
+          <IoLogoLinkedin
             size={24}
-            className="text-2xl hover:text-content-tertiary"
+            className="text-2xl hover:text-content-primary"
           />
         </a>
         <a href={MAIL_URL}>
-          <FaEnvelope
-            size={24}
-            className="text-3x1 hover:text-content-tertiary"
-          />
+          <IoMail size={24} className="text-3x1 hover:text-content-primary" />
         </a>
       </div>
       <div className="flex flex-col gap-2 text-footer items-center">
