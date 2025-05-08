b6386d5afb8fac1134edfd571995ceebb053105d
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-05-08 18:27:00 -0300

    Updates archive to separate projects file

--- src/components/main/DesignArchive/DesignArchive.tsx
+++ src/components/main/DesignArchive/DesignArchive.tsx
@@ -1,50 +1,8 @@
 import ProjectCard from "@/components/ProjectCard";
 import { useState } from "react";
+import projects from "./projects";
 
 const DesignArchive = () => {
-  const projects = [
-    {
-      title: "Developer Portal",
-      company: "AB InBev",
-      description:
-        "A web portal used for mapping ABI's APIs, facilitating integration with partners by enhancing documentation used by technical teams.",
-      link: "/design/abinbev",
-      image: {
-        url: "/images/home/home-devportal.png",
-        alt: "An overview of AB InBev's developer initial page, which mainly displays reasons for using the web portal as a marketing strategy.",
-        width: 40,
-        height: 40,
-      },
-      locked: true,
-    },
-    {
-      title: "Certificate Emission Platform",
-      company: "Ânima Educação",
-      description:
-        "A web platform that is used to generate Bachelor's and Master's certificates for more than 350.000 students in Brazil.",
-      link: "/design/anima-educacao",
-      image: {
-        url: "/images/home/home-animaeducacao.png",
-        alt: "An overview of Anima's initial page, which displays a list of diplomas waiting to be validated by the user.",
-        width: 40,
-        height: 40,
-      },
-    },
-    {
-      title: "Government Website Usability Audit",
-      company: "Secretary of Finance of Minas Gerais",
-      description:
-        "An academic research on the usability of Brazilian public-sector websites using SUS, heuristic evaluation, and UX surveys to identify design issues and user experience possible improvements.",
-      link: "/design/anima-educacao",
-      image: {
-        url: "/images/home/academic-research-overview.png",
-        alt: "Overview of a study on the usability of Brazilian public-sector websites using SUS, heuristics, and UX surveys.",
-        width: 40,
-        height: 40,
-      },
-    },
-  ];
-
   const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
 
   return (
